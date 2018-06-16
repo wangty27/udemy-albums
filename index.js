7e08e33c8276196b7314024decb23b4263c57a31
Terry Wang <terrywang@tianyu.ninja>
2018-06-15 23:23:18 -0400

    styling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Header from './src/components/header';
 // Create a component
 const App = () => {
   return (
-    <Header />
+    <Header headerText={'Albums'}/>
   );
 };
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,34 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
-  return <Text>Albums!</Text>;
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 // Make the component available to other parts of the app
