d839fc1e508f8fdf138587eb317f850d706e1ca4
Terry Wang <terrywang@tianyu.ninja>
2018-06-16 23:43:08 -0400

    card

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
-import React, { Component} from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -11,7 +12,7 @@ class AlbumList extends Component {
   }
 
   renderAlbums() {
-    return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {
