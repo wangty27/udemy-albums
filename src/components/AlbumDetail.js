5212859d934128b0b011712bb1d723808e410fba
Terry Wang <terrywang@tianyu.ninja>
2018-06-17 00:50:59 -0400

    finish

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image, url } = album;
+  const { headerTextStyle, headerThumbnailStyle, thumbnailImageStyle, headerContentStyle, artworkImageStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={headerThumbnailStyle}>
+          <Image style={thumbnailImageStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={artworkImageStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerThumbnailStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  thumbnailImageStyle: {
+    height: 50,
+    width: 50
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  artworkImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
@@ -17,9 +17,9 @@ class AlbumList extends Component {
 
   render() {
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 };
